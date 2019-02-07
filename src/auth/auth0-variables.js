35ef0448dde0cbdc176e093a5b4eee4a24ca982c
Matthew Lant <Matt@Matts-MacBook-Pro-2.local>
2019-02-06 20:27:18 -0700

    up to date

---
+++ src/auth/auth0-variables.js
@@ -0,0 +1,6 @@
+export const AUTH_CONFIG = {
+    clientId: 'wIvsNVXWwRSkX001IewKhGpoKZPH1iaE',
+    domain: 'whiteowl.auth0.com',
+    callbackUrl: 'http://localhost:3000/callback',
+    apiUrl: '{API_IDENTIFIER}'
+  }
